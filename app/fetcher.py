30ef08c942441e6246093f27ffaccd3b0fbcaa2a
Yakir Uzan <yakiruzangreen@gmail.com>
2025-08-21 10:00:03 +0300

    docs: add comments to MongoFetcher class

--- app/fetcher.py
+++ app/fetcher.py
@@ -4,13 +4,16 @@ from pymongo import MongoClient
 
 class MongoFetcher:
     def __init__(self):
+        #יצירת משתני סביבה לקריאת הדאטה בייס
         self.uri = os.getenv("MONGO_URI")
         self.db_name = os.getenv("DB_NAME", "TwitterDB")
         self.collection_name = os.getenv("COLLECTION_NAME", "tweets")
 
+        #יצירת חיבור למסד הנתונים
         self.client = MongoClient(self.uri)
         self.db = self.client[self.db_name]
         self.collection = self.db[self.collection_name]
 
+    #שליפת כל הנתונים מהקולקשיין
     def fetch_all(self):
         return list(self.collection.find())
